6291d5c95f1d4e9a1a2b93de82c295f4352b4523
Sanya__Petruk <oleksandr.petruk@infopulse.com>
2024-11-19 13:27:56 +0200

    add(login): add login functionality

--- src/sceens/LoginUser.tsx
+++ src/sceens/LoginUser.tsx
@@ -2,11 +2,19 @@ import {useState} from "react";
 import {Button} from "@mui/joy";
 import '../styles/createUser.css'
 import {FormInputComponent} from "../components/FormInputComponent";
+import {UserDaoService} from "../services/userDaoService";
+import {UserType} from "../constants/userConsts";
 
 export const LoginUser = () => {
     const [login, setLogin] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-
+    const [error, setError] = useState<string>('');
+    const loginUser = async () => {
+        const userData = await UserDaoService.loginUser({
+            email: login,
+            password: password,
+        }).catch(err => setError('This user is exist'));
+    }
     return (<>
         <div className="login-main">
             <div className="login-main-text">
@@ -17,8 +25,10 @@ export const LoginUser = () => {
                 <FormInputComponent placeholder={'Email'} value={login} setValue={setLogin} type={'email'}/>
                 <FormInputComponent placeholder={'Password'} value={password} setValue={setPassword} type={'password'}/>
                 <div className={'login-main-form-button'}>
-                <Button className={'login-main-form-button-login'} color={'success'} variant={'solid'} onClick={() => console.log()}>Login</Button>
-                <span>Not remember yet <a href={'/register'}>Register!</a></span>
+                    <Button className={'login-main-form-button-login'} color={'success'} variant={'solid'}
+                            onClick={() => loginUser()}>Login</Button>
+                    <p className='error'>{error}</p>
+                    <span>Not remember yet <a href={'/register'}>Register!</a></span>
                 </div>
             </div>
         </div>
--- src/services/userDaoService.ts
+++ src/services/userDaoService.ts
@@ -8,11 +8,22 @@ type CreateUserData = {
     role: number;
 }
 
+type LoginUserData = {
+    email: string;
+    password: string;
+}
+
 const createUser = async (userData: CreateUserData) => {
     const user = await axios.post(`${BASE_URL}/user/create`, userData).then((response) => console.log(response.data));
     return user;
 }
 
+const loginUser = async (userData: LoginUserData) => {
+    const user = await axios.post(`${BASE_URL}/user/login`, userData).then((response) => console.log(response.data));
+    return user;
+}
+
 export const UserDaoService = {
     createUser,
+    loginUser
 }
